c93bfa29c5c4681503f72a838706043259bd2fe4
walterlv <xblero@gmail.com>
2020-05-20 16:11:14 +0800

    Add initial projects.

---
+++ Walterlv.Issues.CSharpNullable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Walterlv.Issues.CSharpNullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = GetNotNullObject();
+            Task.Run(() => Run());
+            void Run() => VerifyNotNull(foo);
+        }
+
+        private static object GetNotNullObject() => new object();
+
+        private static bool VerifyNotNull(object instance) => instance != null;
+    }
+}
